3065f0061c616106d3c8420547c8b514c82c1fdb
modeco46 <60033169+modeco46@users.noreply.github.com>
2025-12-01 16:01:00 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,19 +5,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      // ↓↓↓ ВОТ ЭТА СТРОКА ОБЯЗАТЕЛЬНА ↓↓↓
-      base: "/Jewelry-Ring-Weight-Calculator/",
+      base: './', 
       
       server: {
         port: 3000,
         host: '0.0.0.0',
       },
       plugins: [react()],
-      // Блок define можно даже удалить, если вы не используете API
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
